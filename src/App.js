2f9346b58b22e55e75ffcdc832d53d72c4a01b4c
aridguy <rotimiariyo@gmail.com>
2022-10-03 16:06:18 +0100

    second commit for today on the gallery project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="container-fluid p-5 bg-warning text-white text-center border-none">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+        <input type="search" placeholder="Search all pictures here" className="form-control" />
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+            <div className='col-md-4'>
+              <div className='img-box'></div>
+            </div>
+            <div className='col-md-4'>dfd</div>
+            <div className='col-md-4'>dff</div>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/GetImages.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function GetImages() {
+    return(
+        <div></div>
+    )
+}
+
+export default GetImages;
