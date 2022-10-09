ddf5d4bf9bcaaec3d13294ddcccd0805367457be
aridguy <rotimiariyo@gmail.com>
2022-10-09 22:17:42 +0100

    done here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import GetImages from './GetImages';
 
 function App() {
   return (
     <div>
-
+ 
       <div className="container-fluid p-5 bg-warning text-white text-center border-none">
         <h1>My First Bootstrap Page</h1>
         <p>Resize this responsive page to see the effect!</p>
@@ -14,12 +15,14 @@ function App() {
       <div className='container'>
         <div className='row'>
             <div className='col-md-4'>
-              <div className='img-box'></div>
+              <div className='img-box'>jbj</div>
             </div>
             <div className='col-md-4'>dfd</div>
             <div className='col-md-4'>dff</div>
         </div>
       </div>
+
+      <GetImages />
 
     </div>
   );
--- src/GetImages.js
+++ src/GetImages.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function GetImages() {
-    return(
-        <div></div>
+    const [images, setImages] = useState([])
+    useEffect(() => {
+        const fetchImages = async() => {
+            await fetch("https://www.fb.com/macbrill13")
+            .then((res)=>{console.log(res);})
+            .catch((error)=>{console.log(error);})
+        }
+
+        fetchImages();
+    }, [])
+
+    return (
+        <div>
+            <h1>hello images are fetched here</h1>
+        </div>
     )
 }
 
